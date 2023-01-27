a93e221140426b986cc150d02461d27f807386c4
spnishu <shashankpandey7492@gmail.com>
2023-01-27 14:01:07 +0530

    updated explanation in setup file

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -11,16 +11,19 @@ class TrainingPipelineConfig:
         self.timestamp: str = timestamp
 
 
+
+
+
+
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         try:
             self.database_name="aps"
             self.collection_name="sensor"
-            self.data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME)
-            self.feature_store_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME)
-            self.train_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME)
-            self.test_file_path: str = os.path.join(self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME)
-            self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
+            self.data_ingestion_dir: str = os.path.join(training_pipeline_config.artifact_dir,"data_ingestion")
+            self.feature_store_file_path: str = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path: str = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.test_file_path: str = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
             self.test_size=0.2
         except Exception as e:
             raise SensorException(e,sys)
--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import find_packages,setup
 
 from typing import List
 
-REQUIREMENT_FILE_NAME="requirements.txt"
+# I want to read my requirement.txt file and return list of string
+REQUIREMENT_FILE_NAME="requirements.txt" #Store text file in in variable
 HYPHEN_E_DOT = "-e ." #To trigger setup.py file
 
 
-def get_requirements()->List[str]:
-    
+def get_requirements()->List[str]: #This will return list of string of libraries
+
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()
+        requirement_list = requirement_file.readlines() #Will read each line
+
+    #replace \n from each line since we are reading text file therefore \n will be there and  \n is not a library
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     
+    #replace -e. since it is not a library it is required to trigger setup file
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
